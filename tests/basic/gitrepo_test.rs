a1b29aef5621b87a12ad8a751565d81723ae04c9
Thomas Buckley-Houston <tom@tombh.co.uk>
2022-12-08 22:19:32 -0300

    chore: Use `String` matchers rather than array[]
    Generally speaking I think array[] access is only used in very
    perofrmance critical applications.

--- tests/basic/gitrepo_test.rs
+++ tests/basic/gitrepo_test.rs
@@ -2,18 +2,22 @@ use stele::utils::git::{Repo, GIT_REQUEST_NOT_FOUND};
 
 use crate::common;
 
+const COMMIT: &str = "ed782e08d119a580baa3067e2ea5df06f3d1cd05";
+
+fn blob_to_string(blob: Vec<u8>) -> String {
+    core::str::from_utf8(blob.as_slice()).unwrap().into()
+}
+
 #[test]
 fn test_get_bytes_at_path_when_empty_path_expect_index_html() {
     common::initialize();
     let test_library_path = common::get_test_library_path();
     let repo = Repo::new(&test_library_path, "test", "law-html").unwrap();
-    let actual = repo
-        .get_bytes_at_path("ed782e08d119a580baa3067e2ea5df06f3d1cd05", "")
-        .unwrap();
+    let actual = repo.get_bytes_at_path(COMMIT, "").unwrap();
     let expected = "<!DOCTYPE html>";
-    assert_eq!(
-        &core::str::from_utf8(actual.as_slice()).unwrap()[..15],
-        expected
+    assert!(
+        blob_to_string(actual).starts_with(expected),
+        "doesn't start with {expected}"
     );
 }
 
@@ -22,13 +26,11 @@ fn test_get_bytes_at_path_when_full_path_expect_data() {
     common::initialize();
     let test_library_path = common::get_test_library_path();
     let repo = Repo::new(&test_library_path, "test", "law-html").unwrap();
-    let actual = repo
-        .get_bytes_at_path("ed782e08d119a580baa3067e2ea5df06f3d1cd05", "a/b/c.html")
-        .unwrap();
+    let actual = repo.get_bytes_at_path(COMMIT, "a/b/c.html").unwrap();
     let expected = "<!DOCTYPE html>";
-    assert_eq!(
-        &std::str::from_utf8(actual.as_slice()).unwrap()[..15],
-        expected
+    assert!(
+        blob_to_string(actual).starts_with(expected),
+        "doesn't start with {expected}"
     );
 }
 
@@ -37,13 +39,11 @@ fn test_get_bytes_at_path_when_omit_html_expect_data() {
     common::initialize();
     let test_library_path = common::get_test_library_path();
     let repo = Repo::new(&test_library_path, "test", "law-html").unwrap();
-    let actual = repo
-        .get_bytes_at_path("ed782e08d119a580baa3067e2ea5df06f3d1cd05", "a/b/c")
-        .unwrap();
+    let actual = repo.get_bytes_at_path(COMMIT, "a/b/c").unwrap();
     let expected = "<!DOCTYPE html>";
-    assert_eq!(
-        &std::str::from_utf8(actual.as_slice()).unwrap()[..15],
-        expected
+    assert!(
+        blob_to_string(actual).starts_with(expected),
+        "doesn't start with {expected}"
     );
 }
 
@@ -52,13 +52,11 @@ fn test_get_bytes_at_path_when_omit_index_expect_data() {
     common::initialize();
     let test_library_path = common::get_test_library_path();
     let repo = Repo::new(&test_library_path, "test", "law-html").unwrap();
-    let actual = repo
-        .get_bytes_at_path("ed782e08d119a580baa3067e2ea5df06f3d1cd05", "a/b/d")
-        .unwrap();
+    let actual = repo.get_bytes_at_path(COMMIT, "a/b/d").unwrap();
     let expected = "<!DOCTYPE html>";
-    assert_eq!(
-        &std::str::from_utf8(actual.as_slice()).unwrap()[..15],
-        expected
+    assert!(
+        blob_to_string(actual).starts_with(expected),
+        "doesn't start with {expected}"
     );
 }
 
@@ -68,7 +66,10 @@ fn test_get_bytes_at_path_when_invalid_repo_namespace_expect_error() {
     let test_library_path = common::get_test_library_path();
     let actual = Repo::new(&test_library_path, "xxx", "law-html").unwrap_err();
     let expected = "failed to resolve path";
-    assert_eq!(&format!("{}", actual)[..22], expected);
+    assert!(
+        actual.to_string().contains(expected),
+        "\"{actual}\" doesn't contain {expected}"
+    );
 }
 
 #[test]
@@ -77,7 +78,10 @@ fn test_get_bytes_at_path_when_invalid_repo_name_expect_error() {
     let test_library_path = common::get_test_library_path();
     let actual = Repo::new(&test_library_path, "test", "xxx").unwrap_err();
     let expected = "failed to resolve path";
-    assert_eq!(&format!("{}", actual)[..22], expected);
+    assert!(
+        actual.to_string().contains(expected),
+        "\"{actual}\" doesn't contain {expected}"
+    );
 }
 
 #[test]
@@ -85,9 +89,10 @@ fn test_get_bytes_at_path_when_invalid_path_expect_error() {
     common::initialize();
     let test_library_path = common::get_test_library_path();
     let repo = Repo::new(&test_library_path, "test", "law-html").unwrap();
-    let actual = repo
-        .get_bytes_at_path("ed782e08d119a580baa3067e2ea5df06f3d1cd05", "a/b/x")
-        .unwrap_err();
+    let actual = repo.get_bytes_at_path(COMMIT, "a/b/x").unwrap_err();
     let expected = GIT_REQUEST_NOT_FOUND;
-    assert_eq!(format!("{}", actual), expected);
+    assert!(
+        actual.to_string().contains(expected),
+        "\"{actual}\" doesn't contain {expected}"
+    );
 }
