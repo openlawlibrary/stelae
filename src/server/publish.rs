0aa8c4b41ddbfcff76d7c3ab22ad95b8dde0fe31
n-dusan <nikolic.dusan.dey@gmail.com>
2023-04-03 17:13:37 +0200

    feat(wip): progress towards dynamic routes and dynamic appdata

--- src/server/publish.rs
+++ src/server/publish.rs
@@ -3,7 +3,7 @@
 #![allow(clippy::unused_async)]
 use crate::server::tracing::StelaeRootSpanBuilder;
 use crate::stelae::archive::Archive;
-use actix_web::{get, web, App, HttpRequest, HttpServer};
+use actix_web::{get, web, App, HttpRequest, HttpServer, Route, Scope};
 use std::{collections::HashMap, path::Path, path::PathBuf};
 use tracing_actix_web::TracingLogger;
 /// Global, read-only state
@@ -17,6 +17,11 @@ struct AppState {
 // #[get("/t")]
 async fn index() -> &'static str {
     "Welcome to Publish Server"
+}
+
+
+async fn default() -> &'static str {
+    "Default"
 }
 
 /// Index path for testing purposes
@@ -41,31 +46,23 @@ pub async fn serve_archive(
     let message = "Running Publish Server on a Stelae archive at";
     tracing::info!("{message} '{raw_archive_path}' on http://{bind}:{port}.",);
 
-    let mut state = AppState {
-        archive: Archive {
-            path: archive_path.clone(),
-            stelae: HashMap::new(),
-        },
-    };
-
-    let archive = Archive::parse(archive_path, PathBuf::from(raw_archive_path)).unwrap();
-    dbg!(&archive);
-    //     let root_stele = archive.get_root();
-    //     //TODO: figure out how to access specific stele instead of root stele
-    //     // let stele = state.archive.get_current_stele(Path::new(&raw_archive_path));
-    //     if let Ok(root_stele) = root_stele {
-    //         //load dependencies.json file from stelae root
-    //         // let dependencies = root_stele.get_dependencies().unwrap();
-    //         // for stele_name in dependencies.dependencies.keys() {
-    // //
-    //         // }
-    //     }
+    let archive =
+        Archive::parse(archive_path, PathBuf::from(raw_archive_path)).unwrap_or_else(|_| {
+            tracing::error!("Unable to parse archive at '{raw_archive_path}'.");
+            std::process::exit(1);
+        });
+    let mut state = AppState { archive };
+    //TODO: root stele is a stele from which we began serving the archive
+    // let root = state.archive.get_root().unwrap_or_else(|_| {
+    //     tracing::error!("Unable to determine root Stele.");
+    //     std::process::exit(1);
+    // });
 
     HttpServer::new(move || {
         App::new()
             .wrap(TracingLogger::<StelaeRootSpanBuilder>::new())
             .app_data(web::Data::new(state.clone()))
-            .configure(init)
+            .configure(|cfg| init_routes(cfg, state.clone()))
     })
     .bind((bind, port))?
     .run()
@@ -73,34 +70,61 @@ pub async fn serve_archive(
 }
 
 /// Routes
-fn init(cfg: &mut web::ServiceConfig) {
-    //for stele in archive.stelae.values {
-    // for repository in stele.repositories {
-    //     for scope in scopes {
-
+fn init_routes(cfg: &mut web::ServiceConfig, state: AppState) {
+    // let mut scopes: Vec<Scope> = vec![];
+    // for stele in state.archive.stelae.values() {
+    //     // dbg!(&stele.path);
+    //     if let Some(repositories) = &stele.repositories {
+    //         // dbg!(&repositories);
+    //         for scope in repositories.scopes.iter().flat_map(|s| s.iter()) {
+    //             // dbg!(&scope);
+    //             // let scope = web::scope(scope.as_str());
+    //             for repository in repositories.repositories.values() {
+    //                 let custom = &repository.custom;
+    //                 for route in custom.routes.iter().flat_map(|r| r.iter()) {
+    //                     //ignore routes that start with underscore
+    //                     if route.starts_with("~ _") {
+    //                         continue;
+    //                     }
+    //                     let actix_route = format!("/{{prefix:{}}}", &route);
+    //                     dbg!(&actix_route);
+    //                     dbg!(&scope);
+    //                     let actix_scope = web::scope(scope.as_str())
+    //                         .route(actix_route.as_str(), web::get().to(default));
+    //                     scopes.push(actix_scope);
+    //                 }
+    //             }
+    //         }
     //     }
     // }
-    //}
+    // for scope in scopes {
+    //     cfg.service(scope);
+    // }
+
     {
         let mut smc_hashmap = HashMap::new();
         smc_hashmap.insert("cityofsanmateo".to_owned(), "some value for SMC".to_owned());
-
-        //for each scope:
-        //
-        cfg.service(
-            web::scope("/us/ca/cities/san-mateo")
-                .app_data(web::Data::new(smc_hashmap))
-                .route("/{prefix:_reader/.*}", web::get().to(test))
-                .route("/{pdfs:.*/.*pdf}", web::get().to(index)), // .service(index)
-                                                                  // .service(test),
-        );
-    }
-    {
         let mut dc_hashmap = HashMap::new();
         dc_hashmap.insert("dc".to_owned(), "some value for DC".to_owned());
 
         cfg.service(
-            web::scope("/us/dc").app_data(web::Data::new(dc_hashmap)), // .service(test),
+            web::scope("/us/ca/cities/san-mateo")
+                .service(web::resource("/{prefix:_reader/.*}")
+                // .route("/{prefix:_reader/.*}", web::get().to(test))
+                .app_data(web::Data::new(smc_hashmap))
+                .route(web::get().to(test)))
+                // .route("/{pdfs:.*/.*pdf}", web::get().to(test))
+                // .app_data(web::Data::new(dc_hashmap))
+                .service(web::resource("/{pdfs:.*/.*pdf}").app_data(web::Data::new(dc_hashmap)).route(web::get().to(test))), // .service(index)
+                                                                                                 // .service(test),
         );
     }
+    // {
+    //     let mut dc_hashmap = HashMap::new();
+    //     dc_hashmap.insert("dc".to_owned(), "some value for DC".to_owned());
+
+    //     cfg.service(
+    //         web::scope("/us/dc").app_data(web::Data::new(dc_hashmap)), // .service(test),
+    //     );
+    // }
 }
--- src/stelae/types/repositories.rs
+++ src/stelae/types/repositories.rs
@@ -3,23 +3,25 @@ use std::collections::HashMap;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Repositories {
-    scopes: Option<Vec<String>>,
-    repositories: HashMap<String, Repository>,
+    /// Scopes of the repositories
+    pub scopes: Option<Vec<String>>,
+    /// Data repositories
+    pub repositories: HashMap<String, Repository>,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-struct Repository {
-    custom: Custom,
+pub struct Repository {
+    pub custom: Custom,
 }
 #[derive(Debug, Clone, Deserialize, Serialize)]
-struct Custom {
+pub struct Custom {
     #[serde(rename = "type")]
-    repository_type: Option<String>,
+    pub repository_type: Option<String>,
     #[serde(rename = "allow-unauthenticated-commits")]
-    allow_unauthenticated_commits: Option<bool>,
-    serve: String,
-    routes: Option<Vec<String>>,
+    pub allow_unauthenticated_commits: Option<bool>,
+    pub serve: String,
+    pub routes: Option<Vec<String>>,
     #[serde(rename = "serve-prefix")]
-    scope: Option<String>,
-    is_fallback: Option<bool>,
+    pub scope: Option<String>,
+    pub is_fallback: Option<bool>,
 }
