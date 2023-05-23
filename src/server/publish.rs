8cad7b9b4131ae25fd8c76f0a3572532ebf91422
n-dusan <nikolic.dusan.dey@gmail.com>
2023-05-23 19:38:45 +0200

    wip: shared fallback repository is initialized

--- src/server/publish.rs
+++ src/server/publish.rs
@@ -1,10 +1,11 @@
 //! Serve documents in a Stelae archive.
 #![allow(clippy::exit)]
 #![allow(clippy::unused_async)]
-use crate::server::tracing::StelaeRootSpanBuilder;
 use crate::stelae::archive::Archive;
+use crate::utils::archive::get_name_parts;
 use crate::utils::git::Repo;
 use crate::utils::http::get_contenttype;
+use crate::{server::tracing::StelaeRootSpanBuilder, stelae::stele::Stele};
 use actix_web::{
     get, guard, web, App, HttpRequest, HttpResponse, HttpServer, Resource, Responder, Route, Scope,
 };
@@ -36,14 +37,12 @@ struct RepoState {
     repo: Repo,
     ///Latest or historical
     serve: String,
-    // /Fallback Repository if this one is not found
-    fallback: Option<Box<RepoState>>,
 }
 
-/// Repository to fall back to if the current one is not found
-struct FallbackState {
-    ///Fallback Repository if this one is not found
-    fallback: Box<RepoState>,
+/// Shared, read-only app state
+struct SharedState {
+    /// Repository to fall back to if the current one is not found
+    fallback: Option<RepoState>,
 }
 
 impl fmt::Debug for RepoState {
@@ -57,11 +56,34 @@ impl fmt::Debug for RepoState {
     }
 }
 
+impl fmt::Debug for SharedState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if let &Some(ref fallback) = &self.fallback {
+            write!(
+                f,
+                "Repo for {} in the archive at {}",
+                fallback.repo.name,
+                fallback.repo.path.display()
+            )
+        } else {
+            write!(f, "No fallback repo")
+        }
+    }
+}
+
 impl Clone for RepoState {
     fn clone(&self) -> Self {
         Self {
             repo: self.repo.clone(),
             serve: self.serve.clone(),
+            // fallback: self.fallback.clone(),
+        }
+    }
+}
+
+impl Clone for SharedState {
+    fn clone(&self) -> Self {
+        Self {
             fallback: self.fallback.clone(),
         }
     }
@@ -78,8 +100,13 @@ async fn default() -> &'static str {
 }
 
 /// Serve current document
-async fn serve(req: HttpRequest, data: web::Data<RepoState>) -> impl Responder {
+async fn serve(
+    req: HttpRequest,
+    shared: web::Data<SharedState>,
+    data: web::Data<RepoState>,
+) -> impl Responder {
     dbg!(&data);
+    dbg!(&shared);
     dbg!(&req.path().to_owned());
     let mut path = req.path().to_owned();
     // dbg!(&path);
@@ -132,10 +159,18 @@ pub async fn serve_archive(
         });
     let state = AppState { archive };
     // TODO: intiialize fallback repository
+    let root = state.archive.get_root().unwrap();
+    let shared_state = init_shared_app_state(root);
+    // let shared_state = SharedState { fallback: None };
+    dbg!(&shared_state);
+
     HttpServer::new(move || {
-        App::new()
-            .wrap(TracingLogger::<StelaeRootSpanBuilder>::new())
-            .configure(|cfg| init_routes(cfg, state.clone()))
+        App::new().service(
+            web::scope("")
+                .app_data(web::Data::new(shared_state.clone()))
+                .wrap(TracingLogger::<StelaeRootSpanBuilder>::new())
+                .configure(|cfg| init_routes(cfg, state.clone())),
+        )
     })
     .bind((bind, port))?
     .run()
@@ -147,7 +182,7 @@ fn init_routes(cfg: &mut web::ServiceConfig, mut state: AppState) {
     let mut scopes: Vec<Scope> = vec![];
     // initialize root stele routes and scopes
     let root = state.archive.get_root().unwrap();
-
+    let mut root_scope: Scope = web::scope("");
     // if let &Some(ref repositories) = &root.repositories {
 
     // }
@@ -155,26 +190,12 @@ fn init_routes(cfg: &mut web::ServiceConfig, mut state: AppState) {
 
     for stele in state.archive.stelae.values() {
         if let &Some(ref repositories) = &stele.repositories {
+            // Root Stele
             if stele.get_qualified_name() == root.get_qualified_name() {
-                continue;
-            }
-            for scope in repositories.scopes.iter().flat_map(|s| s.iter()) {
-                // let escaped_scope = regex::escape(scope);
-                // let url_namespace = format!("{{namespace:^{}/.*}}", &scope.as_str());
-                // let url_namespace = format!("{{namespace:({})+}}", &scope.as_str());
-                // dbg!(&url_namespace);
-                // let mut actix_scope = web::scope("/{namespace:us/ca/cities/san-mateo}");
-                let mut actix_scope = web::scope(scope.as_str());
-                // dbg!(&scope);
-                for &(ref name, ref repository) in &repositories.repositories {
+                for &ref repository in &repositories.repositories {
                     let custom = &repository.custom;
                     let repo_state = {
-                        // let mut repo_path = stele
-                        //     .path
-                        //     .clone()
-                        //     .parent()
-                        //     .unwrap()
-                        //     .to_string_lossy();
+                        let name = &repository.name;
                         let mut repo_path = state.archive.path.to_string_lossy().into_owned();
                         repo_path = format!("{repo_path}/{name}");
                         RepoState {
@@ -187,7 +208,66 @@ fn init_routes(cfg: &mut web::ServiceConfig, mut state: AppState) {
                                     .expect("Unable to open Git repository"),
                             },
                             serve: custom.serve.clone(),
-                            fallback: None,
+                        }
+                    };
+                    for route in custom.routes.iter().flat_map(|r| r.iter()) {
+                        //ignore routes in child stele that start with underscore
+                        if route.starts_with("~ _") {
+                            // TODO: append route to root stele scope
+                            continue;
+                        }
+                        let actix_route = format!("/{{prefix:{}}}", &route);
+                        root_scope = root_scope.service(
+                            web::resource(actix_route.as_str())
+                                .route(web::get().to(serve))
+                                .app_data(web::Data::new(repo_state.clone())),
+                        );
+                    }
+                    if let &Some(ref underscore_scope) = &custom.scope {
+                        let actix_underscore_scope = web::scope(underscore_scope.as_str()).service(
+                            web::scope("").service(
+                                web::resource("/{prefix:.*}")
+                                    .route(web::get().to(serve))
+                                    .app_data(web::Data::new(repo_state.clone())),
+                            ),
+                        );
+                        scopes.push(actix_underscore_scope);
+                    }
+                }
+                continue;
+            }
+            //Child Stele
+            for scope in repositories.scopes.iter().flat_map(|s| s.iter()) {
+                // let escaped_scope = regex::escape(scope);
+                // let url_namespace = format!("{{namespace:^{}/.*}}", &scope.as_str());
+                // let url_namespace = format!("{{namespace:({})+}}", &scope.as_str());
+                // dbg!(&url_namespace);
+                // let mut actix_scope = web::scope("/{namespace:us/ca/cities/san-mateo}");
+                let mut actix_scope = web::scope(scope.as_str());
+                // dbg!(&scope);
+                for &ref repository in &repositories.repositories {
+                    let custom = &repository.custom;
+                    let repo_state = {
+                        // let mut repo_path = stele
+                        //     .path
+                        //     .clone()
+                        //     .parent()
+                        //     .unwrap()
+                        //     .to_string_lossy();
+                        let name = &repository.name;
+                        let mut repo_path = state.archive.path.to_string_lossy().into_owned();
+                        repo_path = format!("{repo_path}/{name}");
+                        RepoState {
+                            repo: Repo {
+                                archive_path: state.archive.path.to_string_lossy().to_string(),
+                                path: PathBuf::from(repo_path.clone()),
+                                org: stele.org.clone(),
+                                name: name.to_string(),
+                                repo: Repository::open(repo_path)
+                                    .expect("Unable to open Git repository"),
+                            },
+                            serve: custom.serve.clone(),
+                            // fallback: None,
                         }
                     };
                     for route in custom.routes.iter().flat_map(|r| r.iter()) {
@@ -221,6 +301,8 @@ fn init_routes(cfg: &mut web::ServiceConfig, mut state: AppState) {
     for scope in scopes {
         cfg.service(scope);
     }
+    // Register root stele scope last
+    cfg.service(root_scope);
     // {
     //     let mut smc_hashmap = HashMap::new();
     //     smc_hashmap.insert("cityofsanmateo".to_owned(), "some value for SMC".to_owned());
@@ -263,4 +345,18 @@ fn init_routes(cfg: &mut web::ServiceConfig, mut state: AppState) {
     //         web::scope("/us/dc").app_data(web::Data::new(dc_hashmap)), // .service(test),
     //     );
     // }
+}
+
+/// Initialize the shared application state
+/// Currently shared application state consists of:
+///     - fallback: used as a data repository to resolve data when no other url matches the request
+fn init_shared_app_state(root: &Stele) -> SharedState {
+    let fallback = root.get_fallback_repo().map(|repo| {
+        let (org, name) = get_name_parts(&repo.name).unwrap();
+        RepoState {
+            repo: Repo::new(&root.archive_path, &org, &name).unwrap(),
+            serve: repo.custom.serve,
+        }
+    });
+    SharedState { fallback }
 }
