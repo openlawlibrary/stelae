0b1f736c9034cbc4e3c68111f48f224509a42b73
n-dusan <nikolic.dusan.dey@gmail.com>
2023-06-02 16:31:18 +0200

    fix(utils): default to `text/html` instead of `application/octet-stream`.
    HTML documents with dots in filenames get defaulted to
    application/octet-stream, which is incorrect. Default to text/html.
    Update unit tests which now show correct behavior.

--- src/utils/http.rs
+++ src/utils/http.rs
@@ -1,19 +1,19 @@
 //! The http module helper functions useful for serving http content
-use actix_files::file_extension_to_mime;
+use mime_guess::from_ext as file_extension_to_mime;
+
 use actix_web::http::header::ContentType;
 use std::path::Path;
 
 /// `get_contenttype` uses the file extension to return the `ContentType`
 /// for the content at `path`. If there is no extension, we assume it is
 /// html. If the extension cannot be converted to a str, then we return
-/// octet stream.
+/// HTML.
 #[must_use]
 pub fn get_contenttype(path: &str) -> ContentType {
-    // let mimetype = get_mimetype(blob_path);
     let extension = Path::new(&path)
         .extension()
         .map_or("html", |ext| ext.to_str().map_or("", |ext_str| ext_str));
-    let mime = file_extension_to_mime(extension);
+    let mime = file_extension_to_mime(extension).first_or(mime::TEXT_HTML);
     ContentType(mime)
 }
 
@@ -50,6 +50,22 @@ mod test {
         let cut = get_contenttype;
         let actual = cut("a/b.xml").to_string();
         let expected = String::from("text/xml");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_get_contenttype_when_incorrect_ext_expect_html() {
+        let cut = get_contenttype;
+        let actual = cut("a/b.01").to_string();
+        let expected = String::from("text/html");
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_get_contenttype_when_exe_ext_expect_octet_stream() {
+        let cut = get_contenttype;
+        let actual = cut("a/b.exe").to_string();
+        let expected = String::from("application/octet-stream");
         assert_eq!(expected, actual);
     }
 }
