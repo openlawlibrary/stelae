dbcb43f3aa85a1bc599a91ea88b0c3f4795dc5d5
n-dusan <nikolic.dusan.dey@gmail.com>
2023-11-29 19:20:32 +0100

    refact(test): remove unnecessary test modules
    Tests were moved to basic archive test module

--- tests/api/individual_stele_test.rs
+++
@@ -1,8 +0,0 @@
-use crate::common;
-use crate::common::ArchiveType;
-use actix_web::test;
-
-#[actix_web::test]
-async fn test_something() {
-    common::initialize_archive(ArchiveType::Basic).unwrap();
-}
