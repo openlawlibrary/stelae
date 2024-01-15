1ca238ce92fff9641fc8af9742a302da98aa4cc7
n-dusan <nikolic.dusan.dey@gmail.com>
2024-01-15 16:41:26 +0100

    feat(db): add generic connection over database
    - Introduce a `db` module responsible for managing database
    - Add `connect` function that's generic over SQLite and Postgres

---
+++ src/db/mod.rs
@@ -0,0 +1,37 @@
+//! Database related module.
+
+/// Database initialization.
+pub mod init;
+
+/// Generic Database
+pub struct Database;
+
+/// Database connection.
+pub enum DatabaseConnection {
+    /// SQLite connection.
+    Sqlite(sqlx::SqlitePool),
+    /// Postgres connection.
+    Postgres(sqlx::PgPool),
+}
+
+impl Database {
+    /// Connects to a database.
+    ///
+    /// # Errors
+    /// Errors if connection to database fails.
+    pub async fn connect(db_url: &str) -> anyhow::Result<DatabaseConnection> {
+        let connection = match db_url {
+            url if url.starts_with("sqlite://") => {
+                let pool = sqlx::SqlitePool::connect(url).await?;
+                DatabaseConnection::Sqlite(pool)
+            }
+            url if url.starts_with("postgres://") => {
+                let pool = sqlx::PgPool::connect(url).await?;
+                DatabaseConnection::Postgres(pool)
+            }
+            _ => anyhow::bail!("Unsupported database URL: {}", db_url),
+        };
+
+        Ok(connection)
+    }
+}
