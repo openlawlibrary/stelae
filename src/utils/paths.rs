1086f9826e63eafa5c9467cd4983f74f337ac95f
n-dusan <nikolic.dusan.dey@gmail.com>
2024-01-26 13:37:07 +0100

    refact: rename parameter for `fix_unc_path` fn

--- src/utils/paths.rs
+++ src/utils/paths.rs
@@ -3,15 +3,12 @@ use std::path::{Path, PathBuf};
 /// On Windows removes the `\\?\\` prefix to UNC paths.
 /// For other OS'es just turns the `Path` into a `PathBuf`
 #[must_use]
-pub fn fix_unc_path(res: &Path) -> PathBuf {
+pub fn fix_unc_path(absolute_path: &Path) -> PathBuf {
     if cfg!(windows) {
-        let res_str = res.display().to_string();
-        if res_str.starts_with(r#"\\?"#) {
-            PathBuf::from(res_str.replace(r#"\\?\"#, ""))
-        } else {
-            res.to_path_buf()
+        let absolute_path_str = absolute_path.display().to_string();
+        if absolute_path_str.starts_with(r#"\\?"#) {
+            return PathBuf::from(absolute_path_str.replace(r#"\\?\"#, ""));
         }
-    } else {
-        res.to_path_buf()
     }
+    absolute_path.to_path_buf()
 }
