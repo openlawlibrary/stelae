e6c81fb3c0238cbcc1f8097b919943c00cb82405
n-dusan <nikolic.dusan.dey@gmail.com>
2024-03-19 20:52:30 +0100

    feat(history): add `rdf` submodule
    The purpose of the submodule is to store any/all functions that work
    with rdf. Currently the underlying implementation relies on `sophia` to
    parse the graph and retrieve triples.
    
    Add `namespaces.rs` module that stores rdf namespaces. Currently stores
    the `oll` and `dcterms` ontology. I expected the `dcterms` to be
    supported out-of-the-box, but `sophia` supports only the most frequently
    used namespaces.

---
+++ src/history/rdf/mod.rs
@@ -0,0 +1,4 @@
+//! The rdf module contains helper functions that work with loading, parsing and querying the RDF graph using `sophia`.
+
+/// RDF namespaces for the Stele ontology.
+pub mod namespaces;
---
+++ src/history/rdf/namespaces.rs
@@ -0,0 +1,23 @@
+/// This module contains the RDF namespaces used by Stelae.
+use sophia::api::namespace;
+
+/// Open Law Library ontology.
+pub mod oll {
+    use super::namespace;
+    namespace! {
+        "https://open.law/us/ngo/oll/_ontology/v0.1/ontology.owl#",
+        CollectionVersion,
+        DocumentVersion,
+        docId
+    }
+}
+
+/// Dublin Core Terms ontology.
+pub mod dcterms {
+    use super::namespace;
+
+    namespace! {
+        "http://purl.org/dc/terms/",
+        available
+    }
+}
