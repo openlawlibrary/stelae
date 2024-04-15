08510b22b6acc69dce6b11e59a298c8cb65ee5b8
n-dusan <nikolic.dusan.dey@gmail.com>
2024-04-15 17:23:22 +0200

    refact(db): update sqlx struct for document change composite keys

--- src/db/models/document_change.rs
+++ src/db/models/document_change.rs
@@ -3,8 +3,6 @@ use serde::{Deserialize, Serialize};
 #[derive(sqlx::FromRow, Deserialize, Serialize)]
 /// Model for document change events.
 pub struct DocumentChange {
-    /// Database id.
-    pub id: i32,
     /// Materialized path to the document
     pub doc_mpath: String,
     /// Change status of the document.
@@ -14,8 +12,12 @@ pub struct DocumentChange {
     pub url: String,
     /// Optional reason for the change event.
     pub change_reason: Option<String>,
-    /// Foreign key reference to publication_version id.
-    pub publication_version_id: i32,
+    /// Foreign key reference to the publication name.
+    pub publication: String,
+    /// Foreign key reference to codified date in a publication in %Y-%m-%d format
+    pub version: String,
+    /// Foreign key reference to stele identifier in <org>/<name> format.
+    pub stele: String,
     /// Foreign key reference to document id.
-    pub document_id: i32,
+    pub doc_id: String,
 }
