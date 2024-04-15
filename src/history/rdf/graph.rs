4f18fbccdf559bc1446710719c9dff2509911a55
n-dusan <nikolic.dusan.dey@gmail.com>
2024-04-15 21:20:33 +0200

    refact(rdf): split functionalities for working with rdf graph on literals and iris
    Since the graph queries will vary on the type of information that we
    need, the easiest is to create utility functions that return (s,p,o)
    depending on the queries.

--- src/history/rdf/graph.rs
+++ src/history/rdf/graph.rs
@@ -1,9 +1,11 @@
+/// The helper methods for working with RDF in Stelae.
 use anyhow::Context;
 use sophia::api::graph::Graph;
+use sophia::api::ns::rdf::li;
 use sophia::api::ns::NsTerm;
 use sophia::api::{prelude::*, term::SimpleTerm};
-/// The helper methods for working with RDF in Stelae.
 use sophia::inmem::graph::FastGraph;
+use sophia::inmem::index::TermIndexFullError;
 
 /// Stelae representation of an RDF graph.
 pub struct StelaeGraph {
@@ -29,40 +31,100 @@ impl StelaeGraph {
         predicate: Option<NsTerm>,
         object: Option<NsTerm>,
     ) -> anyhow::Result<String> {
-        let triple = match (subject, predicate, object) {
-            (Some(s), None, None) => {
-                self.g.triples_matching([s], Any, Any).next().context("Did not find a triple matching provided subject in the graph")?
-            },
-            (None, Some(p), None) => {
-                self.g.triples_matching(Any, [p], Any).next().context("Did not find a triple matching provided predicate in the graph")?
-            },
-            (None, None, Some(o)) => {
-                self.g.triples_matching(Any, Any, [o]).next().context("Did not find a triple matching provided object in the graph")?
-            },
-            (Some(s), Some(p), None) => {
-                self.g.triples_matching([s], [p], Any).next().context("Did not find a triple matching provided subject and predicate in the graph")?
-            },
-            (Some(s), None, Some(o)) => {
-                self.g.triples_matching([s], Any, [o]).next().context("Did not find a triple matching provided subject and object in the graph")?
-            },
-            (None, Some(p), Some(o)) => {
-                self.g.triples_matching(Any, [p], [o]).next().context("Did not find a triple matching provided predicate and object in the graph")?
-            },
-            (Some(s), Some(p), Some(o)) => {
-                self.g.triples_matching([s], [p], [o]).next().context("Did not find a triple matching provided subject, predicate and object in the graph")?
-            },
-            (None, None, None) => {
-                anyhow::bail!("No subject, predicate or object provided");
-            }
-        };
-        // let triple = self.g.triples_matching(Any, [predicate], Any).next().context("Did not find a triple matching provided object in the graph")?;
-        let literal = match triple?.o() {
+        let triple = self.get_next_triples_matching(subject, predicate, object)?;
+        let literal = match triple.o() {
             SimpleTerm::LiteralLanguage(literal, _) => literal,
             SimpleTerm::LiteralDatatype(literal, _) => literal,
             _ => {
-                anyhow::bail!("Expected literal language, got - {:?}", triple?.o());
+                anyhow::bail!("Expected literal language, got - {:?}", triple.o());
             }
         };
         Ok(literal.to_string())
+    }
+
+    /// Extract all literals from a triple matching.
+    ///
+    /// # Errors
+    /// Errors if the triple matching the object is not found.
+    /// Errors if the object is not an RDF literal.
+    pub fn all_literals_from_triple_matching(
+        &self,
+        subject: Option<&SimpleTerm>,
+        predicate: Option<NsTerm>,
+        object: Option<NsTerm>,
+    ) -> anyhow::Result<Vec<String>> {
+        Ok(self
+            .literal_from_triple_matching(subject, predicate, object)
+            .into_iter()
+            .map(|t| t)
+            .collect())
+    }
+
+    /// Extract an IRI from a triple matching.
+    ///
+    /// # Errors
+    /// Errors if the triple matching the object is not found.
+    /// Errors if the object is not an RDF IRI.
+    pub fn iri_from_triple_matching<'graph>(
+        &'graph self,
+        subject: Option<&'graph SimpleTerm>,
+        predicate: Option<NsTerm<'graph>>,
+        object: Option<NsTerm<'graph>>,
+    ) -> anyhow::Result<SimpleTerm> {
+        let triple = self.get_next_triples_matching(subject, predicate, object)?;
+        let iri = match triple.o() {
+            SimpleTerm::Iri(literal) => literal,
+            _ => {
+                anyhow::bail!("Expected literal language, got - {:?}", triple.o());
+            }
+        };
+        Ok(SimpleTerm::Iri(iri.clone()))
+    }
+
+    /// Extract subjects from a triple matching a subject.
+    pub fn subjects_from_triples_matching_subject(&self, subject: SimpleTerm) -> Vec<SimpleTerm> {
+        self.g
+            .triples_matching([subject], Any, Any)
+            .into_iter()
+            .filter_map(|t| {
+                let t = t.ok()?;
+                Some(t.s().clone())
+            })
+            .collect()
+    }
+
+    fn get_next_triples_matching<'graph>(
+        &'graph self,
+        subject: Option<&'graph SimpleTerm>,
+        predicate: Option<NsTerm<'graph>>,
+        object: Option<NsTerm<'graph>>,
+    ) -> anyhow::Result<[&'graph SimpleTerm<'_>; 3]> {
+        let triple = match (subject, predicate, object) {
+                (Some(s), None, None) => {
+                    self.g.triples_matching([s], Any, Any).next().context("Did not find a triple matching provided subject in the graph")
+                },
+                (None, Some(p), None) => {
+                    self.g.triples_matching(Any, [p], Any).next().context("Did not find a triple matching provided predicate in the graph")
+                },
+                (None, None, Some(o)) => {
+                    self.g.triples_matching(Any, Any, [o]).next().context("Did not find a triple matching provided object in the graph")
+                },
+                (Some(s), Some(p), None) => {
+                    self.g.triples_matching([s], [p], Any).next().context("Did not find a triple matching provided subject and predicate in the graph")
+                },
+                (Some(s), None, Some(o)) => {
+                    self.g.triples_matching([s], Any, [o]).next().context("Did not find a triple matching provided subject and object in the graph")
+                },
+                (None, Some(p), Some(o)) => {
+                    self.g.triples_matching(Any, [p], [o]).next().context("Did not find a triple matching provided predicate and object in the graph")
+                },
+                (Some(s), Some(p), Some(o)) => {
+                    self.g.triples_matching([s], [p], [o]).next().context("Did not find a triple matching provided subject, predicate and object in the graph")
+                },
+                (None, None, None) => {
+                    anyhow::bail!("No subject, predicate or object provided")
+                }
+            }?;
+        Ok(triple?)
     }
 }
