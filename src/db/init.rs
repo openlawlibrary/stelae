8e87bb2de4d0d7431ab4ec77704d06e73660afa5
n-dusan <nikolic.dusan.dey@gmail.com>
2024-04-18 21:49:53 +0200

    fix: add missing `/` to sqlite db url

--- src/db/init.rs
+++ src/db/init.rs
@@ -10,7 +10,7 @@ use std::path::{Path, PathBuf};
 pub async fn connect(archive_path: &Path) -> anyhow::Result<DatabaseConnection> {
     let db_url = env::var("DATABASE_URL").unwrap_or_else(|_| {
         let sqlite_db_path = &archive_path.join(PathBuf::from(".stelae/db.sqlite3"));
-        format!("sqlite://{}?mode=rwc", sqlite_db_path.to_string_lossy())
+        format!("sqlite:///{}?mode=rwc", sqlite_db_path.to_string_lossy())
     });
     let connection = DatabaseConnection::connect(&db_url).await?;
     tracing::info!("Connected to database");
