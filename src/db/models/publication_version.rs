93b9d87a4bd2d78ab5fcc651c606fe9055e8814b
n-dusan <nikolic.dusan.dey@gmail.com>
2024-04-22 14:46:56 +0200

    fix(db): impl FromRow to map NULL to Option

--- src/db/models/publication_version.rs
+++ src/db/models/publication_version.rs
@@ -1,14 +1,26 @@
 use serde::{Deserialize, Serialize};
+use sqlx::{any::AnyRow, FromRow, Row};
 
-#[derive(sqlx::FromRow, Deserialize, Serialize, Hash, Eq, PartialEq, Clone)]
+#[derive(Deserialize, Serialize, Hash, Eq, PartialEq, Clone)]
 /// Model for a Stele.
 pub struct PublicationVersion {
     /// Date in a publication in %Y-%m-%d format
     pub version: String,
     /// Foreign key reference to publication name.
-    pub name: String,
+    pub publication: String,
     /// Foreign key reference to stele.
     pub stele: String,
     /// Reason for building the publication.
     pub build_reason: Option<String>,
+}
+
+impl FromRow<'_, AnyRow> for PublicationVersion {
+    fn from_row(row: &AnyRow) -> Result<Self, sqlx::Error> {
+        Ok(PublicationVersion {
+            version: row.try_get("version").unwrap(),
+            publication: row.try_get("publication").unwrap(),
+            stele: row.try_get("stele").unwrap(),
+            build_reason: row.try_get("build_reason").ok(),
+        })
+    }
 }
