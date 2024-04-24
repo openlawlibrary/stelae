2a4d10f809d303e75743b64b83570c9f0f192987
n-dusan <nikolic.dusan.dey@gmail.com>
2024-04-24 20:05:34 +0200

    refact: reorganize routing
    - Introduce a `routing` module, intended as a central place
    to map all routing in stelae app. Introduced the `_api` scope
    with the versions endpoint
    - feat(api): initial versions endpoint
    - feat(app): call register_api routing function
    - feat(api): extract stele name from request

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,8 +1,10 @@
-//! Functionality for serving Stelae libraries.
+//! Functionality for serving Stelae archive.
 //!
 //! Currently contains only a git microserver.
 
 pub mod errors;
 pub mod git;
+pub mod history;
 pub mod publish;
+pub mod routes;
 pub mod tracing;
