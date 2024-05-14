4400c15657cf693f022bec07d51871d4011fdf96
n-dusan <nikolic.dusan.dey@gmail.com>
2024-05-14 17:14:24 +0200

    feat: add unit tests for historical messages

--- src/server/api/versions/response/messages.rs
+++ src/server/api/versions/response/messages.rs
@@ -191,3 +191,279 @@ fn messages_between_template(
         },
     )
 }
+
+#[cfg(test)]
+mod tests {
+    use serde_json::json;
+
+    use super::*;
+
+    use std::cmp::Reverse;
+    use std::collections::BTreeMap;
+
+    use super::super::Publication;
+
+    fn publication_to_versions() -> BTreeMap<Reverse<String>, Publication> {
+        let test_data = json!({
+            "2023-12-30": {
+                "active": false,
+                "date": "2023-12-30",
+                "display": "2023-12-30",
+                "name": "2023-12-30",
+                "versions": [
+                    {"date": "2023-12-30", "display": "2023-12-30", "version": 0},
+                    {"date": "2023-12-11", "display": "2023-12-11", "version": 0},
+                    {"date": "2023-11-02", "display": "2023-11-02", "version": 0},
+                    {"date": "2023-10-22", "display": "2023-10-22", "version": 0},
+                    {"date": "2023-08-12", "display": "2023-08-12", "version": 0},
+                    {"date": "2023-08-10", "display": "2023-08-10", "version": 0},
+                    {"date": "2023-06-04", "display": "2023-06-04", "version": 0},
+                    {"date": "2023-01-01", "display": "2023-01-01", "version": 0}
+                ]
+            },
+            "2023-10-22": {
+                "active": false,
+                "date": "2023-10-22",
+                "display": "2023-10-22",
+                "name": "2023-10-22",
+                "versions": [
+                    {"date": "2023-10-22", "display": "2023-10-22", "version": 0},
+                    {"date": "2023-08-12", "display": "2023-08-12", "version": 0},
+                    {"date": "2023-08-10", "display": "2023-08-10", "version": 0},
+                    {"date": "2023-06-04", "display": "2023-06-04", "version": 0},
+                    {"date": "2023-01-01", "display": "2023-01-01", "version": 0}
+                ]
+            }
+        });
+        let map: BTreeMap<Reverse<String>, Publication> =
+            serde_json::from_value(test_data).unwrap();
+        map
+    }
+
+    fn current_publication_name() -> String {
+        "2023-12-30".to_string()
+    }
+
+    #[test]
+    fn test_historical_when_current_publication_expect_no_historical_messages() {
+        let active_publication_name = "2023-12-30".to_string();
+        let current_publication_name = current_publication_name();
+        let publication_to_versions = publication_to_versions();
+        let versions = &publication_to_versions
+            .get(&Reverse(active_publication_name.clone()))
+            .unwrap()
+            .versions;
+        let version_date: Option<String> = None;
+        let compare_to_date: Option<String> = None;
+
+        let cut = historical;
+
+        let actual = cut(
+            &versions,
+            &current_publication_name,
+            &active_publication_name,
+            &version_date,
+            &compare_to_date,
+        );
+        let expected = Historical {
+            publication: None,
+            version: None,
+            comparison: None,
+        };
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn test_historical_when_outdated_publication_expect_publication_message_with_last_update() {
+        let test_cases = vec![
+            None,
+            Some("2023-10-22".to_string()),
+            Some("2024-06-06".to_string()),
+        ];
+
+        for version_date in test_cases {
+            let active_publication_name = "2023-10-22".to_string();
+            let current_publication_name = current_publication_name();
+            let publication_to_versions = publication_to_versions();
+            let versions = &publication_to_versions
+                .get(&Reverse(active_publication_name.clone()))
+                .unwrap()
+                .versions;
+            let compare_to_date: Option<String> = None;
+
+            let cut = historical;
+
+            let actual = cut(
+                &versions,
+                &current_publication_name,
+                &active_publication_name,
+                &version_date,
+                &compare_to_date,
+            );
+            let expected = Historical {
+                publication: Some(publication_message_template(&versions[0].date)),
+                version: None,
+                comparison: None,
+            };
+
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn test_historical_when_outdated_publication_and_outdated_date_expect_publication_and_version_message_with_last_update(
+    ) {
+        let test_cases = vec![
+            ("2023-01-01", "2023-01-01", "2023-06-04"), // first date
+            ("2023-08-12", "2023-08-12", "2023-10-22"), // middle date
+            ("2023-02-02", "2023-01-01", "2023-06-04"), // non-existing date
+        ];
+
+        for (version_date, start_date, end_date) in test_cases {
+            let active_publication_name = "2023-10-22".to_string();
+            let current_publication_name = current_publication_name();
+            let publication_to_versions = publication_to_versions();
+            let versions = &publication_to_versions
+                .get(&Reverse(active_publication_name.clone()))
+                .unwrap()
+                .versions;
+            let compare_to_date: Option<String> = None;
+
+            let cut = historical;
+
+            let actual = cut(
+                &versions,
+                &current_publication_name,
+                &active_publication_name,
+                &Some(version_date.to_string()),
+                &compare_to_date,
+            );
+            let expected = Historical {
+                publication: Some(publication_message_template(&versions[0].date)),
+                version: Some(version_message_template(version_date, start_date, end_date)),
+                comparison: None,
+            };
+
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn test_historical_when_comparing_with_latest_date_expect_historical_message_with_comparison_date(
+    ) {
+        let test_cases = vec![
+            ("2023-10-22", "no updates"),
+            ("2023-08-12", "1 update"),
+            ("2023-08-10", "2 updates"),
+            ("2023-07-01", "3 updates"), // non-existing date
+            ("2023-01-01", "4 updates"),
+            ("2020-01-01", "5 updates"), // non-existing date before creation date
+        ];
+
+        for (version_date, changes) in test_cases {
+            let active_publication_name = "2023-10-22".to_string();
+            let current_publication_name = current_publication_name();
+            let publication_to_versions = publication_to_versions();
+            let versions = &publication_to_versions
+                .get(&Reverse(active_publication_name.clone()))
+                .unwrap()
+                .versions;
+            let compare_to_date = Some("2023-10-22".to_string());
+            let start_date = version_date;
+
+            let cut = historical;
+
+            let actual = cut(
+                &versions,
+                &current_publication_name,
+                &active_publication_name,
+                &Some(version_date.to_string()),
+                &compare_to_date,
+            );
+
+            let expected_comparison_message = messages_between_template(
+                match changes {
+                    "no updates" => 0,
+                    "1 update" => 1,
+                    "2 updates" => 2,
+                    "3 updates" => 3,
+                    "4 updates" => 4,
+                    "5 updates" => 5,
+                    _ => 0,
+                },
+                &format_date(start_date),
+                None,
+            );
+
+            let expected = Historical {
+                publication: Some(publication_message_template(&versions[0].date)),
+                version: None,
+                comparison: Some(expected_comparison_message),
+            };
+
+            assert_eq!(actual, expected);
+        }
+    }
+
+    #[test]
+    fn test_historical_messages_when_comparing_with_non_latest_date_expect_historical_message() {
+        let test_cases = vec![
+            ("2023-12-11", "2023-12-11", "no updates"),
+            ("2023-10-22", "2023-11-02", "1 update"),
+            ("2023-10-22", "2023-12-11", "2 updates"),
+            ("2023-07-01", "2023-12-11", "5 updates"), // non-existing start date
+            ("2023-06-04", "2023-09-11", "2 updates"), // non-existing end date
+            ("2023-07-01", "2023-09-11", "2 updates"), // non-existing start and end date
+            ("2020-01-01", "2023-06-04", "2 updates"), // non-existing start date before creation date
+            ("2020-01-01", "2020-06-04", "no updates"), // non-existing start and end date before creation date
+            ("2020-01-01", "2024-06-04", "8 updates"),  // end date in the future
+            ("2023-07-01", "2024-06-04", "6 updates"), // non-existing start date and end date in the future
+        ];
+
+        for (version_date, compare_to_date, changes) in test_cases {
+            let active_publication_name = "2023-12-30".to_string();
+            let current_publication_name = current_publication_name();
+            let publication_to_versions = publication_to_versions();
+            let versions = &publication_to_versions
+                .get(&Reverse(active_publication_name.clone()))
+                .unwrap()
+                .versions;
+
+            let cut = historical;
+
+            let actual = cut(
+                &versions,
+                &current_publication_name,
+                &active_publication_name,
+                &Some(version_date.to_string()),
+                &Some(compare_to_date.to_string()),
+            );
+
+            let expected_comparison_message = messages_between_template(
+                match changes {
+                    "no updates" => 0,
+                    "1 update" => 1,
+                    "2 updates" => 2,
+                    "3 updates" => 3,
+                    "4 updates" => 4,
+                    "5 updates" => 5,
+                    "6 updates" => 6,
+                    "7 updates" => 7,
+                    "8 updates" => 8,
+                    _ => 0,
+                },
+                &format_date(version_date),
+                Some(format_date(compare_to_date)),
+            );
+
+            let expected = Historical {
+                publication: None,
+                version: None,
+                comparison: Some(expected_comparison_message),
+            };
+
+            assert_eq!(actual, expected);
+        }
+    }
+}
