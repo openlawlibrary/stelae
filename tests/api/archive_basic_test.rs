dc98a40a8233ce3f6a17d13e2a9c8a8d6a74a708
Bojan Galic <bojan.galic99@gmail.com>
2026-01-06 12:45:20 +0100

    tests: add additonal tests for redirect

--- tests/api/archive_basic_test.rs
+++ tests/api/archive_basic_test.rs
@@ -264,3 +264,170 @@ async fn test_redirect_law_html_request_with_incorrect_redirects_json_expect_fai
     let resp = test::call_service(&app, req).await;
     assert_ne!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
 }
+
+#[actix_web::test]
+async fn test_redirect_law_html_and_law_pdf_request_with_redirects_json_expect_success() {
+    let archive_path =
+        common::initialize_archive_without_bare(ArchiveType::Basic(Jurisdiction::Single)).unwrap();
+
+    let html_repo_path: PathBuf = archive_path.path().join("test_org/law-html");
+    let xml_repo_path: PathBuf = archive_path.path().join("test_org/law-pdf");
+
+    let file_content_html = r#"
+    [
+        [
+            "/a/b/c.html",
+            "/"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let file_content_pdf = r#"
+    [
+        [
+            "/not/existing/path/index.pdf",
+            "/example.pdf"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let _ = add_redirects_json_file(&html_repo_path, file_content_html);
+    let _ = add_redirects_json_file(&xml_repo_path, file_content_pdf);
+    let app = common::initialize_app(archive_path.path()).await;
+
+    let request_uri = "/a/b/c.html";
+    let req = test::TestRequest::get().uri(request_uri).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/");
+
+    let request_uri = "/not/existing/path/index.pdf";
+    let req = test::TestRequest::get().uri(request_uri).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/example.pdf");
+}
+
+#[actix_web::test]
+async fn test_redirect_law_html_request_with_duplicated_entries_in_redirects_json_expect_last_entry_to_be_returned(
+) {
+    let archive_path =
+        common::initialize_archive_without_bare(ArchiveType::Basic(Jurisdiction::Single)).unwrap();
+
+    let html_repo_path: PathBuf = archive_path.path().join("test_org/law-html");
+
+    let file_content = r#"
+    [
+        [
+            "/a/b/c.html",
+            "/"
+        ],
+        [
+            "/a/b/c.html",
+            "/a"
+        ],
+        [
+            "/a/b/c.html",
+            "/a/b"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let _ = add_redirects_json_file(&html_repo_path, file_content);
+    let app = common::initialize_app(archive_path.path()).await;
+
+    let request_uri = "/a/b/c.html";
+    let req = test::TestRequest::get().uri(request_uri).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/a/b");
+}
+
+#[actix_web::test]
+async fn test_redirect_law_html_and_law_pdf_request_with_same_entries_in_redirects_json_expect_first_added_entry_to_be_returned(
+) {
+    let archive_path =
+        common::initialize_archive_without_bare(ArchiveType::Basic(Jurisdiction::Single)).unwrap();
+
+    let html_repo_path: PathBuf = archive_path.path().join("test_org/law-html");
+    let xml_repo_path: PathBuf = archive_path.path().join("test_org/law-pdf");
+
+    let file_content_html = r#"
+    [
+        [
+            "/a/b/",
+            "/"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let file_content_pdf = r#"
+    [
+        [
+            "/a/b/",
+            "/example.pdf"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let _ = add_redirects_json_file(&html_repo_path, file_content_html);
+    let _ = add_redirects_json_file(&xml_repo_path, file_content_pdf);
+    let app = common::initialize_app(archive_path.path()).await;
+
+    let request_uri = "/a/b/";
+    let req = test::TestRequest::get().uri(request_uri).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/example.pdf");
+
+    let request_uri = "/a/b/";
+    let req = test::TestRequest::get().uri(request_uri).to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/example.pdf");
+}
--- tests/api/archive_multihost_test.rs
+++ tests/api/archive_multihost_test.rs
@@ -1,12 +1,14 @@
 use crate::{
     archive_testtools::{
-        self,
+        self, add_redirects_json_file,
         config::{ArchiveType, MultihostConfig},
         utils,
     },
     common,
 };
+use actix_http::StatusCode;
 use actix_web::test;
+use std::path::PathBuf;
 
 #[actix_web::test]
 async fn test_resolve_both_guarded_stele_law_html_request_with_full_path_expect_success() {
@@ -161,4 +163,144 @@ async fn test_law_other_data_request_content_with_cycle_expect_other_document_re
             );
         }
     }
+}
+
+#[actix_web::test]
+async fn test_redirect_dependant_stele_law_html_request_with_correct_redirects_json_expect_success()
+{
+    let archive_path =
+        common::initialize_archive_without_bare(ArchiveType::Multihost(MultihostConfig::Public))
+            .unwrap();
+
+    let stelae_1_html_repo_path: PathBuf = archive_path.path().join("stele_1/law-html");
+
+    let file_content = r#"
+    [
+        [
+            "/not/a/good/path",
+            "/"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let _ = add_redirects_json_file(&stelae_1_html_repo_path, file_content);
+    let app = common::initialize_app(archive_path.path()).await;
+
+    let request_uri = "/not/a/good/path";
+    let req = test::TestRequest::get()
+        .uri(request_uri)
+        .insert_header(("X-Current-Documents-Guard", "stele_1/law"))
+        .to_request();
+    let resp = test::call_service(&app, req).await;
+    assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+    let location: &str = resp
+        .headers()
+        .get("Location")
+        .expect("Location header missing")
+        .to_str()
+        .expect("Location header is not valid UTF-8");
+
+    assert_eq!(location, "/");
+
+    // follow redirect
+    let req2 = test::TestRequest::get()
+        .insert_header(("X-Current-Documents-Guard", "stele_1/law"))
+        .uri(location)
+        .to_request();
+    let resp2 = test::call_service(&app, req2).await;
+
+    // assert final response
+    assert!(resp2.status().is_success());
+
+    // test on other dependant stelae
+    let request_uri = "/not/a/good/path";
+    let req3 = test::TestRequest::get()
+        .uri(request_uri)
+        .insert_header(("X-Current-Documents-Guard", "stele_2/law"))
+        .to_request();
+    let resp = test::call_service(&app, req3).await;
+    assert_eq!(resp.status(), StatusCode::NOT_FOUND);
+}
+
+#[actix_web::test]
+async fn test_redirect_dependant_stelae_law_html_requests_with_correct_redirects_json_expect_success(
+) {
+    let archive_path =
+        common::initialize_archive_without_bare(ArchiveType::Multihost(MultihostConfig::Public))
+            .unwrap();
+
+    let stelae_1_html_repo_path: PathBuf = archive_path.path().join("stele_1/law-html");
+    let stelae_2_html_repo_path: PathBuf = archive_path.path().join("stele_2/law-html");
+    let stelae_1_1_html_repo_path: PathBuf = archive_path.path().join("stele_1_1/law-html");
+
+    let file_content = r#"
+    [
+        [
+            "/not/a/good/path",
+            "/"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let file_content1 = r#"
+    [
+        [
+            "/not/a/good/path",
+            "/a/"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let file_content2 = r#"
+    [
+        [
+            "/not/a/good/path",
+            "/a/b"
+        ]
+    ]
+    "#
+    .to_string();
+
+    let _ = add_redirects_json_file(&stelae_1_html_repo_path, file_content);
+    let _ = add_redirects_json_file(&stelae_2_html_repo_path, file_content1);
+    let _ = add_redirects_json_file(&stelae_1_1_html_repo_path, file_content2);
+    let app = common::initialize_app(archive_path.path()).await;
+
+    let cases = [
+        ("stele_1/law", "/"),
+        ("stele_2/law", "/a/"),
+        ("stele_1_1/law", "/a/b"),
+    ];
+
+    for (guard_value, expected_location) in cases {
+        let request_uri = "/not/a/good/path";
+        let req = test::TestRequest::get()
+            .uri(request_uri)
+            .insert_header(("X-Current-Documents-Guard", guard_value))
+            .to_request();
+        let resp = test::call_service(&app, req).await;
+        assert_eq!(resp.status(), StatusCode::TEMPORARY_REDIRECT);
+
+        let location: &str = resp
+            .headers()
+            .get("Location")
+            .expect("Location header missing")
+            .to_str()
+            .expect("Location header is not valid UTF-8");
+
+        assert_eq!(location, expected_location);
+    }
+
+    // test on other dependant stelae
+    let request_uri = "/not/a/good/path";
+    let req3 = test::TestRequest::get()
+        .uri(request_uri)
+        .insert_header(("X-Current-Documents-Guard", "stele_1_2/law"))
+        .to_request();
+    let resp = test::call_service(&app, req3).await;
+    assert_eq!(resp.status(), StatusCode::NOT_FOUND);
 }
