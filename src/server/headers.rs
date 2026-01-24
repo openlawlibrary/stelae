46885b280850a75dd44299f78e2bc1da0bf1b310
Bojan Galic <bojan.galic99@gmail.com>
2026-01-24 16:04:47 +0100

    fix: namespace in doctest

--- src/server/headers.rs
+++ src/server/headers.rs
@@ -53,7 +53,7 @@ pub const HTTP_E_TAG: &str = "ETAG";
 /// # Example
 ///
 /// ```rust
-/// use stelae::server::api::archive::matches_if_none_match;
+/// use stelae::server::headers::matches_if_none_match;
 /// let etag = "\"abc123\"";
 /// assert!(matches_if_none_match("\"abc123\"", etag));
 /// assert!(matches_if_none_match("\"xyz\", \"abc123\"", etag));
